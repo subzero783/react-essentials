f0e7e8943dcb6eedf6f028c3cf1f5a598e86c73d
Gustavo <gusta1296@gmail.com>
2021-09-26 22:49:35 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(){
+  return(
+    <header>
+      <h1>Gustavo's Garage</h1>
+    </header>
+  );
+}
+
+function Main(){
+  return(
+    <section>
+      <p>We serve the most delicious food around.</p>
+    </section>
+  );
+}
+
+function Footer(){
+  return(
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
