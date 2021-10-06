eaa8888b8c94f0145d74a9232bd6fd32e05f194a
Gustavo Amezcua <GustavoA@innovision.local>
2021-10-06 13:56:04 -0700

    left at video Fetching data with Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import './App.css'
 
 
@@ -15,17 +15,46 @@ function RegularComponent(){
 
 function App() {
 
-  const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  // const [emotion, setEmotion] = useState("happy");
+  // const [secondary, setSecondary] = useState("tired");
+
+  // useEffect(()=>{
+  //   console.log(`It's ${emotion} around here!`)
+  // }, [emotion])
+  
+  // useEffect(()=>{
+  //   console.log(`It's ${secondary} around here!`)
+  // }, [secondary])
+
+  // const [checked, setChecked] = useState(false)
+
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  )
+
 
   return(
     <>
-      <h1>Current emotion is {emotion}.</h1>
-      <button onClick={()=>setEmotion('happy')}>Happy</button>
-      <button onClick={()=>setEmotion('frustrated')}>Frustrate</button>
-      <button onClick={()=>setEmotion('enthusiastic')}>Enthuse</button>
+      <input 
+        type="checkbox" 
+        onChange={toggle}
+      />
+      <p>
+        {checked ? "checked" : "not checked"}
+      </p>
     </>
   )
+
+  // return(
+  //   <>
+  //     <h1>Current emotion is {emotion} and {secondary}.</h1>
+  //     <button onClick={()=>setEmotion('happy')}>Make Happy</button>
+  //     <button onClick={()=>setSecondary('crabby')}>Make Crabby</button>
+  //     <button onClick={()=>setEmotion('frustrated')}>Frustrate</button>
+  //     <button onClick={()=>setEmotion('enthusiastic')}>Enthuse</button>
+  //   </>
+  // )
 
   // return(
   //   <>
