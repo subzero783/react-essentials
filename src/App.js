6bc1cbdbdd13e0420e9d9c3752d132c9c1822887
Gustavo Amezcua <GustavoA@innovision.local>
2021-10-15 17:43:12 -0700

    left at last video section

--- src/App.js
+++ src/App.js
@@ -15,35 +15,39 @@ function RegularComponent(){
 
 function App({login}) {
 
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  // const [data, setData] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState(null)
 
-  useEffect(()=>{
-    if(!login) return
-    setLoading(true)
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response)=> response.json())
-      .then(setData)
-      .then(()=>setLoading(false))
-      .catch(setError)
-  }, [login])
+  // useEffect(()=>{
+  //   if(!login) return
+  //   setLoading(true)
+  //   fetch(`https://api.github.com/users/${login}`)
+  //     .then((response)=> response.json())
+  //     .then(setData)
+  //     .then(()=>setLoading(false))
+  //     .catch(setError)
+  // }, [login])
 
-  if(loading) return <h1>Loading...</h1>
+  // if(loading) return <h1>Loading...</h1>
 
-  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  // if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
 
-  if(!data) return null
+  // if(!data) return null
 
-  return(
-    <div>
-      <h1>
-        {data.name}
-      </h1>
-      <p>{data.location}</p>
-      <img alt={data.login} src={data.avatar_url} />
-    </div>
-  )
+  // return(
+  //   <div>
+  //     <h1>
+  //       {data.name}
+  //     </h1>
+  //     <p>{data.location}</p>
+  //     <img alt={data.login} src={data.avatar_url} />
+  //   </div>
+  // )
+
+  
+    
+  
 
   // const [emotion, setEmotion] = useState("happy");
   // const [secondary, setSecondary] = useState("tired");
@@ -98,6 +102,12 @@ function App({login}) {
   // }else{
   //   return <RegularComponent/>
   // }
+
+  return(
+    <div>
+      <h1>Hello React Testing Library</h1>
+    </div>
+  )
 }
 
 export default App
